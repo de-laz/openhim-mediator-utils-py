1e1eb9f316033d3d6d6a387d45423da74d35d121
Lazola Sifuba <sifubalazola@gmail.com>
2018-08-04 14:00:30 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openhim_mediator_utils",
+    version="0.0.1",
+    author="Lazola Sifuba",
+    author_email="sifubalazola@gmail.com",
+    description="A utility library for build openHIM mediators",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/de-laz/openhim-mediator-utils-py",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Intended Audience :: Developers"
+    )
+)
